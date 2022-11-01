48fbfb22a7d3dccac55b37afdc10d7aa5549cdcf
harinder24 <hss022002@gmail.com>
2022-11-01 15:33:26 -0700

    f
    f

--- EJS_harinder/public/client.js
+++ EJS_harinder/public/client.js
@@ -4,9 +4,8 @@ $(document).ready(function () {
         event.preventDefault()
         let name = document.getElementById("#name");
         let email = document.getElementById("#email")
-        let password = document.getElementById("#password");
 
-        if(name.value.length == 0 || email.value.length == 0 ||password.value.length == 0){
+        if(name.value.length == 0 || email.value.length == 0){
             alert("Some fields are empty")
         }
         else{
@@ -15,7 +14,6 @@ $(document).ready(function () {
                 name : name.value,
                 email : email.value,
                 date: Date(),
-                password : password.value
             }
             
             $.post("/request", (customerData),
